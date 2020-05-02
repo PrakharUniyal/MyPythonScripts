aa68b7f406da4753c987fea99a611885e65aa7e1
Prakhar Uniyal <b18128@students.iitmandi.ac.in>
2020-05-02 11:31:18 +0530

    Update and rename wscraping.py to webscraping.py

--- webscraping.py
+++ webscraping.py
@@ -1,6 +1,9 @@
+#Boilerplate code for web scraping.
+
 from selenium import webdriver
 driver=webdriver.Chrome()
-driver.get('http://iitmandi.co.in/?fbclid=IwAR3X6SS3Zj26ksPLTVaZgIQWwdr2_EBuke8oeHd4ROwLPCgZ_5oXPtNBHa4')
+url = ""
+driver.get(url)
 someText=driver.find_element_by_id('body-content')
 print(someText.text)
 driver.close()
