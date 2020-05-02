d72d31168172a62b9125c4b7b92a9e1bf9a5fdbb
Prakhar Uniyal <b18128@students.iitmandi.ac.in>
2020-05-02 11:54:37 +0530

    Update poisson_dist_prob.py

--- poisson_dist_prob.py
+++ poisson_dist_prob.py
@@ -1,4 +1,11 @@
 #Program to find the Probability of k>n(some number) in a poisson distribution with a given mean(λ).
+'''
+The Poisson distribution is the discrete probability distribution of the number of times of an events occurring in a given time period, 
+given the average number of times the event occurs over that time period.
+
+The following program calculates the probability of the event occuring more than n times, given that on average it occurs λ times.
+'''
+
 from math import exp
 
 #Factorial function
